758f9e64a68a5149e84c3cd9e1379b9147922324
Hailey Cantu <hmcantu@outlook.com>
2026-06-11 16:45:20 -0500

    feat: replace placeholder asset with high-res 3D icon group on Intro title block

--- client/src/pages/Intro/Intro.tsx
+++ client/src/pages/Intro/Intro.tsx
@@ -3,16 +3,24 @@ import "./Intro.css";
 import introImg1 from "../../assets/intro-1.png";
 import introImg2 from "../../assets/intro-2.png";
 import introImg3 from "../../assets/intro-3.png";
+import decorativeElement from "../../assets/DecorativeElement.png"; 
 
 export default function Intro() {
   const navigate = useNavigate();
 
   return (
     <div className="intro">
-      <h1 className="intro__title">Welcome to Mesh AI</h1>
+      <h1 className="intro__title">
+        Welcome to Mesh AI
+        <img 
+          src={decorativeElement} 
+          alt="" 
+          className="intro__title-decoration" 
+          aria-hidden="true" 
+        />
+      </h1>
       
       <div className="intro__grid">
-        {/* Card 1 */}
         <div className="intro__card">
           <img src={introImg1} alt="Documents vector" className="intro__card-icon" />
           <p className="intro__card-text">
@@ -20,7 +28,6 @@ export default function Intro() {
           </p>
         </div>
 
-        {/* Card 2 */}
         <div className="intro__card">
           <img src={introImg2} alt="Folder management vector" className="intro__card-icon" />
           <p className="intro__card-text">
@@ -28,7 +35,6 @@ export default function Intro() {
           </p>
         </div>
 
-        {/* Card 3 */}
         <div className="intro__card">
           <img src={introImg3} alt="Chat sparkles vector" className="intro__card-icon" />
           <p className="intro__card-text">
